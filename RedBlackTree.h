16cc6ac7c02f6e6a5c79872b726bdca781074804
Daniel Rivas <drivas8891@gmail.com>
2024-03-02 16:57:41 -0800

    Add project files.

---
+++ RedBlackTree.h
@@ -0,0 +1,36 @@
+#ifndef REDBLACKTREE_H
+#define REDBLACKTREE_H
+
+enum Color { RED, BLACK };
+
+class RedBlackTree {
+private:
+	RedBlackNode* root;
+
+	void rotateLeft(RedBlackNode*& node);
+	void rotateRight(RedBlackNode*& node);
+	void fixInsertRBT(RedBlackNode*& node);
+	void fixDeleteRBT(RedBlackNode*& node);
+	RedBlackNode* insertBST(RedBlackNode*& rootNode, RedBlackNode*& insertNode);
+	RedBlackNode* deleteBST(RedBlackNode*& insertNode, int data);
+
+public:
+	RedBlackTree() : root(nullptr) {}
+	
+	void insert(const int& val);
+	void deleteByVal(int val);
+	void inorder();
+	void preorder();
+};
+
+struct RedBlackNode {
+	int data;
+	bool color;
+	RedBlackNode* left;
+	RedBlackNode* right;
+	RedBlackNode* parent;
+
+	RedBlackNode(int data) : data(data), color(RED), left(nullptr), right(nullptr), parent(nullptr) {}
+};
+
+#endif
